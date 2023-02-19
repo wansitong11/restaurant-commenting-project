aa712628b5cc9250229986b386a2c5465d38f697
万思桐 <940258641@qq.com>
2023-02-18 22:35:40 -0500

    Commit

--- src/main/java/com/restaurant_commenting/HmDianPingApplication.java
+++ src/main/java/com/restaurant_commenting/HmDianPingApplication.java
@@ -6,10 +6,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @MapperScan("com.restaurant_commenting.mapper")
 @SpringBootApplication
-public class HmDianPingApplication {
+public class RestaurantCommentingApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(HmDianPingApplication.class, args);
+        SpringApplication.run(RestaurantCommentingApplication.class, args);
     }
 
 }
